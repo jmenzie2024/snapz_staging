e6a774b067d76352a576ae485e234aef2162f8a2
ankit2093tiwari <39435198+ankit2093tiwari@users.noreply.github.com>
2025-06-16 17:38:33 +0530

    Update Test.js

--- components/Test.js
+++ components/Test.js
@@ -9,46 +9,42 @@ const Test = ({
   setAll,
   setInitial17,
   setInitial20,
-  setInitial23
+  setInitial23,
 }) => {
   const [arrayOfCat, setArrayOfCat] = useState([]);
   const [filterr, setFilter] = useState([]);
   const router = useRouter();
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
+    const token = sessionStorage.getItem("token");
     fetchQuestions3(token);
   }, []);
 
   const fetchQuestions3 = async (token) => {
-      try {
-        const response = await fetch(
-          process.env.API_URL+"categories/filter3",
-          {
-            credentials: "include",
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        const data = await response.json();
-       
-        if (data.error) {
-          localStorage.clear();
-          router.push("/");
-        }
-        setFilter(data);
-        
-      } catch (error) {
-        console.error("Error fetching categories:", error);
+    try {
+      const response = await fetch(process.env.API_URL + "categories/filter3", {
+        credentials: "include",
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = await response.json();
+
+      if (data.error) {
+        sessionStorage.clear();
+        router.push("/");
       }
-    };
+      setFilter(data);
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  };
 
   const renderCategories = (categories) => {
     let newArrayOfCat = [];
-    
+
     categories.forEach((category) => {
       newArrayOfCat.push(category);
 
@@ -67,28 +63,29 @@ const Test = ({
     if (filterr.length > 0) {
       var topCategory = filterr.find((category) => category.id === 327);
       // console.log('top category', topCategory)
-      let data = topCategory.children.filter((item) => item.name =='Snapz QB 2017 NEC') 
+      let data = topCategory.children.filter(
+        (item) => item.name == "Snapz QB 2017 NEC"
+      );
       // let data1 = topCategory.children.find((item) => item.name.startsWith('Snapz QB'));
-      console.log('top category data', data);
+      console.log("top category data", data);
 
       if (topCategory) {
         const allCategories = renderCategories(topCategory.children);
         // console.log('allCategories', allCategories);
-        
+
         setArrayOfCat(allCategories);
       }
     }
   }, [filterr]);
 
   useEffect(() => {
-    
     if (arrayOfCat.length === 497) {
-      const cat1 = arrayOfCat.slice(9, 101);
-      const cat2 = arrayOfCat.slice(101, 227);
-      const cat3 = arrayOfCat.slice(227, 364);
-      const cat4 = arrayOfCat.slice(364, 497);
-      const allCat = arrayOfCat.slice(9, 497);
-      
+      const cat1 = arrayOfCat.slice(9, 99);
+      const cat2 = arrayOfCat.slice(99, 225);
+      const cat3 = arrayOfCat.slice(225, 362);
+      const cat4 = arrayOfCat.slice(362, 493);
+      const allCat = arrayOfCat.slice(9, 493);
+
       setCategory14(cat1);
       setCategory17(cat2);
       setCategory20(cat3);
@@ -102,48 +99,44 @@ const Test = ({
   }, [arrayOfCat]);
 
   useEffect(() => {
-   
     if (arrayOfCat.length === 498) {
-      const cat1 = arrayOfCat.slice(9, 101);
-      const cat2 = arrayOfCat.slice(101, 228);
-      const cat3 = arrayOfCat.slice(228, 365);
-      const cat4 = arrayOfCat.slice(365, 495);
-      const allCat = arrayOfCat.slice(9, 495);   
-      
+      const cat1 = arrayOfCat.slice(9, 99);
+      const cat2 = arrayOfCat.slice(99, 226);
+      const cat3 = arrayOfCat.slice(226, 363);
+      const cat4 = arrayOfCat.slice(363, 494);
+      const allCat = arrayOfCat.slice(9, 494);
+
       setCategory14(cat1);
       setCategory17(cat2);
       setCategory20(cat3);
       setCategory23(cat4);
       setAll(allCat);
-  
+
       setInitial17(cat2);
       setInitial20(cat3);
       setInitial23(cat4);
     }
   }, [arrayOfCat]);
 
-
   useEffect(() => {
-    
     if (arrayOfCat.length === 499) {
-      const cat1 = arrayOfCat.slice(9, 101);
-      const cat2 = arrayOfCat.slice(101, 229);
-      const cat3 = arrayOfCat.slice(229, 366);
-      const cat4 = arrayOfCat.slice(366, 496);
-      const allCat = arrayOfCat.slice(9, 496);   
-      
+      const cat1 = arrayOfCat.slice(9, 99);
+      const cat2 = arrayOfCat.slice(99, 227);
+      const cat3 = arrayOfCat.slice(227, 364);
+      const cat4 = arrayOfCat.slice(364, 495);
+      const allCat = arrayOfCat.slice(9, 495);
+
       setCategory14(cat1);
       setCategory17(cat2);
       setCategory20(cat3);
       setCategory23(cat4);
       setAll(allCat);
-  
+
       setInitial17(cat2);
       setInitial20(cat3);
       setInitial23(cat4);
     }
   }, [arrayOfCat]);
 };
-
 
 export default Test;
